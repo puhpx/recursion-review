e727c9050ef83ce8e50fee4de247a246f5a831d6
puhpx <chuckthedeveloper@gmail.com>
2022-06-03 20:32:55 -0400

    getElementsByClassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,29 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+
+  // store node with className
+  // check if current item in document has a className of (className)
+  // if current node classList contains classname
+  // push/add to stored nodes
+  // if children nodes exist or are greater than 0
+  // recursion
+  // return stored nodes
+
+  var nodeArr = [];
+  //   access the DOM (document)
+  var nodes = function(node) {
+
+    if (Object.values(node.classList).includes(className)) {
+      nodeArr.push(node);
+    }
+    for (var i = 0; i < node.children.length; i++) {
+      nodes(node.children[i]);
+    }
+  };
+
+  nodes(document.body);
+
+  return nodeArr;
 };
